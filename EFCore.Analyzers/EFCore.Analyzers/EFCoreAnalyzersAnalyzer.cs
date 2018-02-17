6d1e5aa7001201337da236b1dfe89851a17f936f
Andrew Peters <anpete@microsoft.com>
2018-02-16 16:40:00 -0800

    WIP

--- EFCore.Analyzers/EFCore.Analyzers/EFCoreAnalyzersAnalyzer.cs
+++ EFCore.Analyzers/EFCore.Analyzers/EFCoreAnalyzersAnalyzer.cs
@@ -1,8 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -18,36 +18,72 @@ namespace EFCore.Analyzers
         private static readonly DiagnosticDescriptor _diagnosticDescriptor
             = new DiagnosticDescriptor(
                 Id,
-                "SQL expression contains interpolated strings that will not be parameterized. Review for SQL injection vulnerability.",
-                "SQL expression contains interpolated strings that will not be parameterized. Review for SQL injection vulnerability.",
+                "Possible SQL injection vulnerability.",
+                "The SQL expression passed to '{0}' embeds data that will not be parameterized. Review for SQL injection vulnerability.",
                 "Security",
                 DiagnosticSeverity.Warning,
                 isEnabledByDefault: true);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
             => ImmutableArray.Create(_diagnosticDescriptor);
 
-        public override void Initialize(AnalysisContext context)
+        public override void Initialize(AnalysisContext analysisContext)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.SimpleMemberAccessExpression);
+            analysisContext.RegisterSyntaxNodeAction(
+                AnalyzeSimpleMemberAccessExpressionSyntaxNode, SyntaxKind.SimpleMemberAccessExpression);
         }
 
-        private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        private static void AnalyzeSimpleMemberAccessExpressionSyntaxNode(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext)
         {
-            var memberAccessExpressionSyntax = (MemberAccessExpressionSyntax)context.Node;
+            if (!(syntaxNodeAnalysisContext.Node is MemberAccessExpressionSyntax memberAccessExpressionSyntax))
+            {
+                return;
+            }
+
+            if (!(memberAccessExpressionSyntax.Parent is InvocationExpressionSyntax invocationExpressionSyntax))
+            {
+                return;
+            }
+
             var identifierValueText = memberAccessExpressionSyntax.Name.Identifier.ValueText;
 
-            if (identifierValueText == "FromSql" 
-                || identifierValueText == "ExecuteSqlCommand"
-                || identifierValueText == "ExecuteSqlCommandAsync")
+            if (!(identifierValueText == "FromSql"
+                  || identifierValueText == "ExecuteSqlCommand"
+                  || identifierValueText == "ExecuteSqlCommandAsync"))
             {
-                
-                
-                //var diagnostic = Diagnostic.Create(Rule, memberAccessExpressionSyntax.Locations[0], methodSymbol.Name);
+                return;
+            }
 
-                Console.WriteLine();
+            var symbolInfo
+                = syntaxNodeAnalysisContext.SemanticModel
+                    .GetSymbolInfo(memberAccessExpressionSyntax, syntaxNodeAnalysisContext.CancellationToken);
 
-                //context.ReportDiagnostic(diagnostic);
+            if (symbolInfo.Symbol is IMethodSymbol methodSymbol
+                && (methodSymbol.ContainingType.Name == "RelationalQueryableExtensions"
+                    || methodSymbol.ContainingType.Name == "RelationalDatabaseFacadeExtensions"))
+            {
+                var sqlArgumentExpressionSyntax = invocationExpressionSyntax.ArgumentList.Arguments[0].Expression;
+
+                switch (sqlArgumentExpressionSyntax)
+                {
+                    case LiteralExpressionSyntax _:
+                        return;
+                    case InterpolatedStringExpressionSyntax _:
+                        return;
+                }
+
+                if (sqlArgumentExpressionSyntax.DescendantNodes()
+                    .Any(sn => sn is InterpolatedStringExpressionSyntax interpolatedStringExpressionSyntax
+                        && interpolatedStringExpressionSyntax.DescendantNodes().Any(n => n is InterpolationSyntax)))
+                {
+                    var diagnostic
+                        = Diagnostic.Create(
+                            _diagnosticDescriptor,
+                            memberAccessExpressionSyntax.GetLocation(),
+                            identifierValueText);
+
+                    syntaxNodeAnalysisContext.ReportDiagnostic(diagnostic);
+                }
             }
         }
     }
